461270546670643d290d81b17b2afb3aba1ae5a9
ilenych <ilenych6@gmail.com>
2020-06-13 01:17:05 +0500

    (main.js) Created recreate field and hide ship on my field

--- js/main.js
+++ js/main.js
@@ -1,26 +1,31 @@
 import { Field } from "./Field.js";
 import { Game } from "./Game.js";
 import { field } from "./Enum.js";
+import { status } from "./Enum.js";
 
 // Add listener on main div
-const main = document.getElementById("main");
-main.addEventListener("click", createField);
+const newGame = document.getElementById("newGame");
+newGame.addEventListener("click", createField);
 
 // Add listener on first and second fields
 const firstField = document.getElementById(field.first);
 const secondField = document.getElementById(field.second);
 
+const recreate = document.getElementById("restart");
+recreate.addEventListener("click", recreateField);
 /**
  * Craete field
  */
 function createField() {
+  document.getElementById("newGame").style.display = "none";
+  recreate.style.display = "block";
   // Create 2 fields
   let mf = new Field();
   const myField = mf.generateField();
   let ef = new Field();
   const enemyField = ef.generateField();
 
-  console.log("Create fields");
+  console.log(myField);
 
   for (let field of [
     [myField, firstField, 1],
@@ -38,10 +43,18 @@ function createField() {
       }
     }
     // For each cell set new class list
-    for (let cell of field[0].cells) {
-      document
-        .getElementById(`f${field[2]}x${cell.x}y${cell.y}`)
-        .classList.add(`${cell.status}`);
+    if (field[0] === myField) {
+      for (let cell of field[0].cells) {
+        document
+          .getElementById(`f${field[2]}x${cell.x}y${cell.y}`)
+          .classList.add(`${cell.status}`);
+      }
+    } else {
+      for (let cell of field[0].cells) {
+        document
+          .getElementById(`f${field[2]}x${cell.x}y${cell.y}`)
+          .classList.add(`${status.empty}`);
+      }
     }
   }
   // Remove click
@@ -49,4 +62,10 @@ function createField() {
   // Start game
   let sp = new Game(myField, field.first, enemyField, field.second);
   sp.start();
+}
+
+function recreateField() {
+  firstField.innerHTML = "";
+  secondField.innerHTML = "";
+  createField();
 }
